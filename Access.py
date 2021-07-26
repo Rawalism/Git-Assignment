4926c6af47950553b48d7d73d6160caa9092dd32
Santosh <santoshkumarjherat@gmail.com>
2021-07-26 22:27:33 +0500

    Added my all files

---
+++ Access.py
@@ -0,0 +1,8 @@
+from array import *
+
+myarray = array('i',[10,20,30,40,50])
+
+print("Accessing array elememt...")
+print (myarray[0])
+print (myarray[2])
+print (myarray[4])
---
+++ Even and Odd Numbers.py
@@ -0,0 +1,7 @@
+#Printing even and odd Numbers
+for num in range(1,10):
+    if num %2 == 0:
+        print("Even Numbers:",num)
+
+    else:
+        print("Odd Numbers:",num)
